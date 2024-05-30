20093fc51c3aba1a8f441bbf48cc0156dd1818e4
BronnyPRO <fyodorshchegolev@gmail.com>
2024-05-30 21:49:27 +0700

    Updated models, admin.py. Added views, urls, index.html

--- university/admin.py
+++ university/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from university.models import Students
+from university.models import Students, Subjects, Scores
 from django.contrib import admin
 
 
 admin.site.register(Students, admin.ModelAdmin)
+admin.site.register(Subjects, admin.ModelAdmin)
+admin.site.register(Scores, admin.ModelAdmin)
--- university/views.py
+++ university/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+from collections import defaultdict
+
+from django.views.generic.base import TemplateView
+
+from university.models import Scores
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        scores = Scores.objects.all()
+        student_scores = defaultdict(dict)
+
+        subjects = set()
+        for score in scores:
+            subject_name = score.subject.name
+            subjects.add(subject_name)
+            student_scores[score.student][subject_name] = score.value
+
+        subjects = sorted(subjects)
+        student_statistics = [
+            {
+                'student': student,
+                'scores': [f'{scores[subject]:.1f}' for subject in subjects]
+            }
+            for student, scores in student_scores.items()
+        ]
+        context.update(
+            {
+                'subjects': subjects,
+                'student_statistics': student_statistics
+            }
+        )
+        return context
+
