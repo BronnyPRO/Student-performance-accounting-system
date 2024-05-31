cc34efb92424adc0cd7dd3fb747f162809e00f85
BronnyPRO <fyodorshchegolev@gmail.com>
2024-05-31 15:25:16 +0700

    Added statistics.

--- university/views/index_view.py
+++ university/views/index_view.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-
 from collections import defaultdict
 
 from statistics import mean
 
 from django.views.generic.base import TemplateView
 
-from university.models import Scores
+from university.models import Subjects, Scores
 
 
 class IndexView(TemplateView):
@@ -32,10 +30,36 @@ class IndexView(TemplateView):
             }
             for student, scores in student_scores.items()
         ]
+
+        average_scores = [
+            {
+                'subject': subject,
+                'average_score': f'{mean([scores[subject] for _, scores in student_scores.items()]):.1f}'
+            }
+            for subject in subjects
+        ]
+
+        best_student = student_statistics[0]['student']
+        average_score = float(student_statistics[0]['average_score'])
+        for student in student_statistics[1:]:
+            if float(student['average_score']) > average_score:
+                best_student = student['student']
+                average_score = float(student['average_score'])
+
+        worse_student = student_statistics[0]['student']
+        average_score = float(student_statistics[0]['average_score'])
+        for student in student_statistics[1:]:
+            if float(student['average_score']) < average_score:
+                worse_student = student['student']
+                average_score = float(student['average_score'])
+
         context.update(
             {
                 'subjects': subjects,
-                'student_statistics': student_statistics
+                'student_statistics': student_statistics,
+                'average_scores': average_scores,
+                'best_student': best_student,
+                'worse_student': worse_student
             }
         )
         return context
